fc39ef10f165200cb8352a07d6babff017406001
Tobias Schranz <tobias.g.schranz@gmail.com>
2025-01-03 08:43:53 +0100

    add winner check

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 const page = "https://www.win2day.at/promotions-gewinnspiele/gewinner-des-tages";
 const winnerTextPrefix = "Heute ist die win2day Userin oder der win2day User mit dem Nickname ";
+const usernameToCheck = process.env.MORPH_USERNAME_TO_CHECK?.toLowerCase();
 const winnerUsernameLength = 3;
 
 const response = await fetch(page);
@@ -10,6 +11,18 @@ const html = await response.text();
 const winnerUsernameIndex = html.indexOf(winnerTextPrefix);
 const winnerUsernameStart = winnerUsernameIndex + winnerTextPrefix.length;
 
-const winnerUsername = html.substring(winnerUsernameStart, winnerUsernameStart + winnerUsernameLength);
+const winnerUsername = html.substring(winnerUsernameStart, winnerUsernameStart + winnerUsernameLength).toLowerCase();
 
 console.log("The username of today's winner starts with: " + winnerUsername);
+
+if(!usernameToCheck) {
+  console.log("Please set the MORPH_USERNAME_TO_CHECK environment variable to check if you are the winner.");
+  process.exit(1);
+}
+
+if (usernameToCheck.includes(winnerUsername)) {
+  console.log("You might be the winner of today's win2day promotion!");
+} else {
+  console.log("You have *not* won today's win2day promotion.");
+}
+
