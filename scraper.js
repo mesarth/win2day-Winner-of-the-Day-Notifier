a290a1da57e4943343a50300db666caea29e5007
Tobias Schranz <tobias.g.schranz@gmail.com>
2025-01-03 14:54:02 +0100

    add telegram notification

--- scraper.js
+++ scraper.js
@@ -3,10 +3,30 @@ const winnerTextPrefix = "Heute ist die win2day Userin oder der win2day User mit
 const usernameToCheck = process.env.MORPH_USERNAME_TO_CHECK?.toLowerCase();
 const winnerUsernameLength = 3;
 
+const sendTelegramNotification = async (message) => {
+  const telegramBotToken = process.env.MORPH_TELEGRAM_BOT_TOKEN;
+  const telegramChatId = process.env.MORPH_TELEGRAM_CHAT_ID;
+  const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: telegramChatId,
+      text: message,
+      parse_mode: "HTML"
+    }),
+  });
+  if (!response.ok) {
+    throw new Error(`Failed to send Telegram notification: ${response.statusText}`);
+  }
+  console.log("Telegram notification sent successfully.");
+}
+
 const response = await fetch(page);
 const html = await response.text();
-
-// find the prefix in the html and get the winner
 
 const winnerUsernameIndex = html.indexOf(winnerTextPrefix);
 const winnerUsernameStart = winnerUsernameIndex + winnerTextPrefix.length;
@@ -21,7 +41,8 @@ if(!usernameToCheck) {
 }
 
 if (usernameToCheck.includes(winnerUsername)) {
-  console.log("You might be the winner of today's win2day promotion!");
+  console.log("You might be the winner of today's Win2Day promotion!");
+  await sendTelegramNotification(`<b>Win2day Daily Promotion Winner</b>\nYou might be the winner of today's win2day promotion! The username starts with: <i>${winnerUsername}</i>\n<a href="${page}">${page}</a>`);
 } else {
   console.log("You have *not* won today's win2day promotion.");
 }
